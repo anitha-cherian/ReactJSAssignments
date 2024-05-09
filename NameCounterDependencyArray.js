3cd1431f3e62bd931ef12c1e3ac8d877aaa4963f
anitha-cherian <anitha.kozhichal@gmail.com>
2024-05-09 23:56:46 +0530

    Add files via upload

--- NameCounterDependencyArray.js
+++ NameCounterDependencyArray.js
@@ -12,14 +12,36 @@ function NameCounterDependencyArray() {
      console.log(counter);
  
     }
-    
-  
+    //useEffect with no dependency array
     useEffect(() => {
-      console.log('Counter state changed:', counter);
-      console.log('Name state changed:', name);
-    });
-  
-    return (
+        console.log('Counter state changed:', counter);
+        console.log('Name state changed:', name);
+      });
+    //useEffect` with an empty dependency array (`[]`
+    useEffect(()=>{ 
+        // dislay name and counter in console.
+        console.log('This meaage from empty useEfect,Name: ',name);
+        console.log('This meaage from empty useEfect,Counter: ',counter);
+
+    },[]);
+  //define `useEffect` with `name` as the dependency and print the name to console with custom message
+    useEffect(()=>{
+        console.log('This message is from useEffectName: ',name);
+    },[name]);
+ //define `useEffect` with `counter` as the dependency and print the name to console with custom message
+    
+    useEffect(()=>{
+        console.log('This message is from useEffectCounter: ',counter);
+    },[counter]);
+// /useEffect` with both `name` and `counter` as dependencies, print both name and counter to console
+    useEffect(()=>{
+        console.log('This message from useEffect Name and Counter, Name: ',name);
+        console.log('This message from useEffect Name and Counter, Counter: ',counter);
+    },[name,counter]);
+
+    
+
+     return (
       <div>
         <h1>Name and Counter</h1>
             <div className={styles.inputGroup}>
